5e7597d27c79b580f613a214f57c4b2c0b306fe5
Kshitij <kshitijkapoor0611@gmail.com>
2023-03-18 22:43:58 +0530

    improved focus

--- assets/index.js
+++ assets/index.js
@@ -6,10 +6,18 @@ textarea.addEventListener('input', function() {
     card1.style.background = 'rgba(19, 19, 19, 0.8)';   
     card1.style.border = '1px solid rgba(255, 255, 255, 0.35)';
   } else {
-    // Reset the styles if the textarea is empty
     card1.style.background = '';
     card1.style.transform = '';
     card1.style.transition = '';
     card1.style.border = '';
   }
 });
+textarea.onfocus = function() {
+    card1.style.transform = 'translateY(-0.8em)';
+    card1.style.transition = '0.3s';
+  };
+  
+  textarea.onblur = function() {
+    card1.style.transform = '';
+    card1.style.transition = '';
+  };

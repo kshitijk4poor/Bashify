9da06ce3a084bf61eec8fff6f4dbf9d2ce6c8f4f
Kshitij <kshitijkapoor0611@gmail.com>
2023-03-18 22:40:09 +0530

    added focus

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,15 @@
-const button = document.querySelector(".card-1");
+const textarea = document.querySelector('textarea');
+const card1 = document.querySelector('.card-1');
 
-const readout = document.querySelector("p");
-
-button.addEventListener("mousemove", (e) => {
-  const { x, y } = button.getBoundingClientRect();
-  button.style.setProperty("--x", e.clientX - x);
-  button.style.setProperty("--y", e.clientY - y);
+textarea.addEventListener('input', function() {
+  if (textarea.value !== '') {
+    card1.style.background = 'rgba(19, 19, 19, 0.8)';   
+    card1.style.border = '1px solid rgba(255, 255, 255, 0.35)';
+  } else {
+    // Reset the styles if the textarea is empty
+    card1.style.background = '';
+    card1.style.transform = '';
+    card1.style.transition = '';
+    card1.style.border = '';
+  }
 });
